462d7d694912fe4fcb118730814e95be7e7da49f
Shreea Kapoor <skapoor@tavisca.com>
2018-09-29 17:07:36 +0530

    TweetsAPI Implemented

---
+++ TweetsAPI/TweetsAPI/Controllers/TweetController.cs
@@ -0,0 +1,56 @@
+﻿using Aerospike.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using TweetsAPI.Models;
+
+namespace TweetsAPI.Controllers
+{
+    public class TweetController : ApiController
+    {
+        [HttpGet]
+        //[Route("api/Tweet/GetTweetsbyId/{id}")]
+        public List<Record> GetTweetsbyId([FromUri]String[] ids)
+        {
+            Record record;
+            var client = new AerospikeClient("18.235.70.103", 3000);
+            string nameSpace = "AirEngine";
+            string setName = "Shreea";
+            List<Record> records = new List<Record>();
+            int counter = 0;
+            while (counter<ids.Length)
+            {
+                var key = new Key(nameSpace, setName, ids[counter]);
+                record = client.Get(new WritePolicy(), key);
+                records.Add(record);
+                counter++;
+            }
+            return records;
+        }
+        [HttpPut]
+        [Route("api/Tweet/Update/")]
+        public void EditTweets([FromBody] TweetsData updatedetails)
+        {
+            var client = new AerospikeClient("18.235.70.103", 3000);
+            string nameSpace = "AirEngine";
+            string setName = "Shreea";
+            var key = new Key(nameSpace, setName, updatedetails.tweet_id);
+            client.Put(new WritePolicy(), key, new Bin(updatedetails.bin_name,updatedetails.update_value));
+        }
+        [HttpPut]
+        [Route("api/Tweet/Delete/")]
+        public void DeleteTweets([FromUri]String[] ids)
+        {
+            var client = new AerospikeClient("18.235.70.103", 3000);
+            string nameSpace = "AirEngine";
+            string setName = "Shreea";
+            var key = new Key(nameSpace, setName, ids[0]);
+            client.Delete(new WritePolicy(), key);
+        }
+    }
+}
---
+++ TweetsAPI/TweetsAPI/Models/TweetsData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TweetsAPI.Models
+{
+    public class TweetsData
+    {
+        public string tweet_id { get; set; }
+        public string bin_name { get; set; }
+        public string update_value { get; set; }
+
+    }
+}
